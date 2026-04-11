de1d2bd2e381d076c9565fd5d15d29e8cd957546
Kiryl Kvit <kiryl.kvit@gmail.com>
2026-04-11 13:00:08 +0200

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
     defaultConfig {
         applicationId = "app.mihon"
 
-        versionCode = 45
-        versionName = "1.11.0"
+        versionCode = 46
+        versionName = "1.11.1"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getLatestCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getLatestCommitSha()}\"")
