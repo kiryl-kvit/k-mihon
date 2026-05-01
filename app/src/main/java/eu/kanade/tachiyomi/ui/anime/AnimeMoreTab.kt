c02a23ac88a064eede1cee3a2de63091f4e88ff9
Kiryl Kvit <kiryl.kvit@gmail.com>
2026-05-01 20:32:28 +0200

    add download queue

--- app/src/main/java/eu/kanade/tachiyomi/ui/anime/AnimeMoreTab.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/anime/AnimeMoreTab.kt
@@ -4,6 +4,7 @@ import androidx.compose.animation.graphics.res.animatedVectorResource
 import androidx.compose.animation.graphics.res.rememberAnimatedVectorPainter
 import androidx.compose.animation.graphics.vector.AnimatedImageVector
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -23,13 +24,21 @@ import eu.kanade.domain.ui.UiPreferences
 import eu.kanade.presentation.anime.more.AnimeMoreScreen
 import eu.kanade.presentation.util.Tab
 import eu.kanade.tachiyomi.R
+import eu.kanade.tachiyomi.data.anime.download.AnimeDownloadManager
 import eu.kanade.tachiyomi.ui.category.CategoryScreen
+import eu.kanade.tachiyomi.ui.download.DownloadQueueScreen
 import eu.kanade.tachiyomi.ui.setting.SettingsScreen
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.launch
 import mihon.feature.profiles.core.ProfileManager
 import mihon.feature.profiles.ui.ProfilePickerScreen
 import mihon.feature.profiles.ui.handleProfileShortcut
 import mihon.feature.support.SupportUsScreen
+import tachiyomi.core.common.util.lang.launchIO
 import tachiyomi.i18n.MR
 import tachiyomi.presentation.core.i18n.stringResource
 import uy.kohesive.injekt.Injekt
@@ -61,10 +70,13 @@ data object AnimeMoreTab : Tab {
         val screenModel = rememberScreenModel { AnimeMoreScreenModel() }
         val profileManager = remember { Injekt.get<ProfileManager>() }
         val uiPreferences = remember { Injekt.get<UiPreferences>() }
+        val downloadQueueState by screenModel.downloadQueueState.collectAsState()
 
         AnimeMoreScreen(
+            downloadQueueStateProvider = { downloadQueueState },
             incognitoMode = screenModel.incognitoMode,
             onIncognitoModeChange = { screenModel.incognitoMode = it },
+            onClickDownloadQueue = { navigator.push(DownloadQueueScreen) },
             onClickCategories = { navigator.push(CategoryScreen()) },
             onClickDataAndStorage = { navigator.push(SettingsScreen(SettingsScreen.Destination.DataAndStorage)) },
             onClickProfiles = {
@@ -85,8 +97,36 @@ data object AnimeMoreTab : Tab {
 }
 
 private class AnimeMoreScreenModel(
+    private val animeDownloadManager: AnimeDownloadManager = Injekt.get(),
     basePreferences: BasePreferences = Injekt.get(),
 ) : ScreenModel {
 
     var incognitoMode by basePreferences.incognitoMode.asState(screenModelScope)
+
+    private var _downloadQueueState: MutableStateFlow<AnimeDownloadQueueState> =
+        MutableStateFlow(AnimeDownloadQueueState.Stopped)
+    val downloadQueueState: StateFlow<AnimeDownloadQueueState> = _downloadQueueState.asStateFlow()
+
+    init {
+        screenModelScope.launchIO {
+            combine(
+                animeDownloadManager.isRunning,
+                animeDownloadManager.queueState,
+            ) { isRunning, downloadQueue -> Pair(isRunning, downloadQueue.size) }
+                .collectLatest { (isDownloading, downloadQueueSize) ->
+                    val pendingDownloadExists = downloadQueueSize != 0
+                    _downloadQueueState.value = when {
+                        !pendingDownloadExists -> AnimeDownloadQueueState.Stopped
+                        !isDownloading -> AnimeDownloadQueueState.Paused(downloadQueueSize)
+                        else -> AnimeDownloadQueueState.Downloading(downloadQueueSize)
+                    }
+                }
+        }
+    }
+}
+
+sealed interface AnimeDownloadQueueState {
+    data object Stopped : AnimeDownloadQueueState
+    data class Paused(val pending: Int) : AnimeDownloadQueueState
+    data class Downloading(val pending: Int) : AnimeDownloadQueueState
 }
