2a79ac7c341076f21bc888f7d5087d98e1765363
Kiryl Kvit <kiryl.kvit@gmail.com>
2026-05-30 14:32:08 +0200

    add anime duration badge

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
     defaultConfig {
         applicationId = "app.mihon"
 
-        versionCode = 75
-        versionName = "2.8.1"
+        versionCode = 76
+        versionName = "2.8.2"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getLatestCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getLatestCommitSha()}\"")
