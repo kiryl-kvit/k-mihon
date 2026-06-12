7f308e9fc3a507dacca9814b57cfffb40d6a48bf
Kiryl Kvit <kiryl.kvit@gmail.com>
2026-06-12 18:12:41 +0200

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
     defaultConfig {
         applicationId = "app.mihon"
 
-        versionCode = 79
-        versionName = "2.9.2"
+        versionCode = 80
+        versionName = "2.9.3"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getLatestCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getLatestCommitSha()}\"")
